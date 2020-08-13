867818aa2d03b94024b1f7cfe6e681b4a5e95e7d
Jenise Ruehle <jela07@gmail.com>
2020-08-13 20:31:10 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,13 +1,9 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-    
-  def get_first_song
-    self.songs 
-  end 
   
   def get_genre_of_first_song
-    self.get_first_song.genre
+    self.songs.first.genre
   end
 
   def song_count
